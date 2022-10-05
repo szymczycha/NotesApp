bf95a3b8143f9807b0ce0cb4c2b5b36e90b14855
szymczycha <szymczycha266@gmail.com>
2022-10-05 14:29:32 +0200

    Lekcja3 swietnie sie bawie, nie wiem jak usunac ten folder :)

--- app/src/main/java/com/example/szymonapp005/AlbumsActivity.java
+++ app/src/main/java/com/example/szymonapp005/AlbumsActivity.java
@@ -1,21 +1,80 @@
 package com.example.szymonapp005;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AlbumsActivity extends AppCompatActivity {
     private ListView folderList;
+    private LinearLayout addFolderButton;
+    private ArrayList<String> array;
+    private File pic;
+    private void reloadArray(){
+        array.clear();
+        File[] files = pic.listFiles(); // tablica plików
+        for (File file : files){
+            array.add(file.getName());
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
+        pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES );
+        array = new ArrayList<>();
         folderList = findViewById(R.id.folderList);
-        String[] array = new String[]{"wynik 1","wynik 2","wynik 3"};
+        addFolderButton = findViewById(R.id.add_folder);
+        addFolderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(AlbumsActivity.this);
+                alert.setTitle("Dodaj folder");
+                alert.setMessage("Nazwa:");
+                //tutaj input
+                EditText input = new EditText(AlbumsActivity.this);
+                alert.setView(input);
+                //teraz butony jak poprzednio i
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        File newDir = new File(pic, input.getText().toString());
+                        newDir.mkdir();
+//                        reloadArray();
+                        array.add(input.getText().toString());
+                    }
+
+                });
+                alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //wyświetl which
+                    }
+                });
+                alert.show();
+            }
+        });
+        File dir = new File(pic, "Ludzie");
+        dir.mkdir();
+        File dir2 = new File(pic, "Miejsca");
+        dir2.mkdir();
+        File dir3 = new File(pic, "Rzeczy");
+        dir3.mkdir();
+        File[] files = pic.listFiles(); // tablica plików
+        for (File file : files){
+            array.add(file.getName());
+        }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 AlbumsActivity.this, // tzw Context
                 R.layout.folder_item, // nazwa pliku xml naszego wiersza na liście
@@ -26,7 +85,38 @@ public class AlbumsActivity extends AppCompatActivity {
         folderList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("tag", "click");
 
+            }
+        });
+
+        folderList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("tag", "long");
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(AlbumsActivity.this);
+                alert.setTitle("Uwaga!");
+                alert.setMessage("Na pewno usunąć?");
+                //teraz butony jak poprzednio i
+                alert.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("tag", i+"");
+                        File delFile = new File(pic, array.get(i));
+                        delFile.delete();
+//                        reloadArray();
+                        array.remove(array.get(i));
+
+                    }
+
+                });
+                alert.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alert.show();
+                return true;
             }
         });
     }
--- app/src/main/java/com/example/szymonapp005/MainActivity.java
+++ app/src/main/java/com/example/szymonapp005/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.szymonapp005;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout cameraButton;
     private LinearLayout albumsButton;
     private LinearLayout collageButton;
     private LinearLayout networkButton;
-
+    public void checkPermission(String permission, int requestCode) {
+        // jeśli nie jest przyznane to zażądaj
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+        } else {
+            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100);
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 100);
     }
 }
