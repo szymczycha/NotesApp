67c44a89c2b3ce973fc1b30f5d3f824460ce1b00
szymczycha <szymczycha266@gmail.com>
2022-10-19 14:34:30 +0200

    Lekcja3 BRUUUH2

---
+++ app/src/main/java/com/example/szymonapp005/SingleImage.java
@@ -0,0 +1,14 @@
+package com.example.szymonapp005;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_image);
+    }
+}
